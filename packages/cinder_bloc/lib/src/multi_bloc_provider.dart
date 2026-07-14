95d7e4b8f3e2d0a5fa53ff66d07f187bc34b1d4f
eukalpia <120781817+eukalpia@users.noreply.github.com>
2026-07-14 12:48:24 +0500

    remove unused MultiBlocProvider imports

--- packages/cinder_bloc/lib/src/multi_bloc_provider.dart
+++ packages/cinder_bloc/lib/src/multi_bloc_provider.dart
@@ -1,5 +1,3 @@
-import 'package:cinder/cinder.dart';
-import 'package:cinder_nested/nested.dart';
 import 'package:cinder_provider/provider.dart';
 
 /// Merges multiple bloc providers into one widget tree.
