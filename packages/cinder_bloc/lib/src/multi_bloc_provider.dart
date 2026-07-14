9badb68fe1cebeafa884de41ce0034bd6e438a32
eukalpia <120781817+eukalpia@users.noreply.github.com>
2026-07-14 12:46:18 +0500

    clean MultiBlocProvider API

--- packages/cinder_bloc/lib/src/multi_bloc_provider.dart
+++ packages/cinder_bloc/lib/src/multi_bloc_provider.dart
@@ -5,8 +5,8 @@ import 'package:cinder_provider/provider.dart';
 /// Merges multiple bloc providers into one widget tree.
 class MultiBlocProvider extends MultiProvider {
   MultiBlocProvider({
-    Key? key,
-    required List<SingleChildWidget> providers,
-    required Widget child,
-  }) : super(key: key, providers: providers, child: child);
+    super.key,
+    required super.providers,
+    required super.child,
+  });
 }
